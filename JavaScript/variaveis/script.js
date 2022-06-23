b3fb7558540fd11c1de19549d7b6db997d836ff1
TrevoYex <estevam.neto07@gmail.com>
2022-06-23 02:07:52 -0300

    primeiro aula JS origamid

---
+++ JavaScript/variaveis/script.js
@@ -0,0 +1,19 @@
+var nome = 'Estevam';
+var idade = 22;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Neto', 
+    cidade = 'Fortaleza';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir)
