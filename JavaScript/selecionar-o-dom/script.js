0134b3b95136940c8f7931d97aac19bcef4cd29a
TrevoYex <estevam.neto07@gmail.com>
2022-08-24 19:20:03 -0300

    animais fantasticos parte do HTML e CSS feitos

---
+++ JavaScript/selecionar-o-dom/script.js
@@ -0,0 +1,19 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document
+.getElementsByClassName('grid-section');
+
+console.log(gridSection[0]);
+
+
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const linkInternos = document.querySelector("[href^='#']");
+console.log(linkInternos.href);
+
+
+const animaisImg = document.querySelectorAll('.animais img')
+console.log(animaisImg[1])
