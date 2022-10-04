31ef401ab39b75b01804894746219fe5ee6835af
TrevoYex <estevam.neto07@gmail.com>
2022-10-04 15:33:10 -0300

    Javascript - String - parte 2 e final

--- JavaScript/string/script.js
+++ JavaScript/string/script.js
@@ -1,42 +1,75 @@
-// const comida = 'Pizza';
-// const agua = new String('Agua');
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
 
-// console.log(comida.length);
-
-// const frase = 'A melhor comida';
-
-// console.log(frase[frase.length - 1]);
-// console.log(frase.charAt(frase.length - 1));
-
-const frase = 'A melhor linguagem é ';
-const linguagem = 'Javascript';
-
-const fraseFinal = frase.concat(linguagem, '!!');
-
-
-const fruta = 'Banana';
-const listaFrutas = 'Melancia, Banana, Laranja';
-
-console.log(listaFrutas.includes(fruta));
-
-console.log(fruta.startsWith('Ba'));
-console.log(fruta.endsWith('na'));
-
-
-const transacao1 = 'Depósito de cliente';
-const transacao2 = 'Depósito de fornecedor';
-const transacao3 = 'Taxa de camisas';
-
-console.log(transacao1.slice(0, 3));
-
-console.log(fruta.indexOf('n'));
-console.log(fruta.lastIndexOf('na'));
-
-
-const preco = 'R$ 99,00';
-
-const listaPrecos = ['R$ 99', 'R$ 199', 'R$ 12000'];
-
-listaPrecos.forEach((item) => {
-    console.log(item.padStart(20, '-'));
-});
+const transacoes = [
+    {
+      descricao: 'Taxa do Pão',
+      valor: 'R$ 39',
+    },
+    {
+      descricao: 'Taxa do Mercado',
+      valor: 'R$ 129',
+    },
+    {
+      descricao: 'Recebimento de Cliente',
+      valor: 'R$ 99',
+    },
+    {
+      descricao: 'Taxa do Banco',
+      valor: 'R$ 129',
+    },
+    {
+      descricao: 'Recebimento de Taxa Cliente',
+      valor: 'R$ 49',
+    },
+  ];
+  
+  let taxaTotal = 0;
+  let recebimentoTotal = 0;
+  transacoes.forEach((item) => {
+    const numeroLimpo = +item.valor.replace('R$ ', '');
+    if (item.descricao.slice(0, 4) === 'Taxa') {
+      taxaTotal += numeroLimpo;
+    } else if(item.descricao.slice(0, 4) === 'Rece') {
+      recebimentoTotal += numeroLimpo;
+    }
+  })
+  console.log(taxaTotal)
+  console.log(recebimentoTotal)
+  
+  
+  // Retorne uma array com a lista abaixo
+  const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+  
+  const arrayTransportes = transportes.split(';');
+  console.log(arrayTransportes);
+  
+  // Substitua todos os span's por a's
+  let html = `<ul>
+                  <li><span>Sobre</span></li>
+                  <li><span>Produtos</span></li>
+                  <li><span>Contato</span></li>
+                </ul>`;
+  
+  html = html.split('span').join('a');
+  
+  console.log(html);
+  
+  // Retorne o último caracter da frase
+  const frase = 'Melhor do ano!';
+  
+  console.log(frase.slice(-1));
+  
+  // Retorne o total de taxas
+  const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+  
+  let taxasTotal = 0;
+  transacoes2.forEach((item) => {
+    item = item.toLowerCase().trim().slice(0,4);
+  
+    if(item === 'taxa')
+      taxasTotal++
+  })
+  
+  console.log(taxasTotal)
+  
