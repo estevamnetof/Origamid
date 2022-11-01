935a4b0771bea2a8783f393bb3aef3f0e49cb653
TrevoYex <estevam.neto07@gmail.com>
2022-11-01 14:07:01 -0300

    Javascript - objetos - array e iteração - exercício

--- JavaScript/objetos-array-iteracao/script.js
+++ JavaScript/objetos-array-iteracao/script.js
@@ -1,17 +1,22 @@
 // Selecione cada curso e retorne uma array
 // com objetos contendo o título, descricao,
 // aulas e horas de cada curso
-const cursos = document.querySelectorAll('.cursos');
+const cursos = document.querySelectorAll('.curso');
 const arrayCursos = Array.from(cursos);
 
-console.log(cursos);
-console.log(arrayCursos);
+// console.log(cursos);
+// console.log(arrayCursos);
 
 const objetosCurso = arrayCursos.map((curso) => {
     const titulo = curso.querySelector('h1').innerText;
-    console.log(curso);
+    const descricao = curso.querySelector('p').innerText;
+    const aulas = curso.querySelector('.aulas').innerText;
+    const horas = curso.querySelector('.horas').innerText;
     return {
-        titulo: titulo
+        titulo,
+        descricao,
+        aulas,
+        horas
     }
 })
 
@@ -20,10 +25,18 @@ console.log(objetosCurso);
 // Retorne uma lista com os
 // números maiores que 100
 const numeros = [3, 44, 333, 23, 122, 322, 33];
+const maioresQue100 = numeros.filter(n => n > 100);
+
+console.log(maioresQue100)
 
 // Verifique se Baixo faz parte
 // da lista de instrumentos e retorne true
 const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
+const possuiBaixo = instrumentos.some((item) => {
+  return item = 'Baixo'
+})
+
+console.log(possuiBaixo);
 
 
 // Retorne o valor total das compras
