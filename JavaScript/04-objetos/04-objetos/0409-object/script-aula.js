9223348d3d5b49557629210ed790419cecbee8cd
TrevoYex <estevam.neto07@gmail.com>
2022-11-07 23:32:31 -0300

    Javascript - object finalizado

--- JavaScript/04-objetos/04-objetos/0409-object/script-aula.js
+++ JavaScript/04-objetos/04-objetos/0409-object/script-aula.js
@@ -1,8 +1,24 @@
-// const pessoa = new Object({
-//   nome: 'André'
-// })
+const pessoa = new Object({
+  nome: 'André',
+})
 
-// console.log(pessoa)
+// document.write(pessoa);
+console.log(pessoa);
+
+// const carro = {
+//   marca: 'Marca',
+//   rodas: 4,
+//   acelerar() {
+//     return this.marca + ' acelerou'
+//   },
+//   buzinar() {
+//     return this.marca + ' buzinou'
+//   }
+// }
+
+// const honda = Object.create(carro);
+// honda.marca = 'Honda';
+
 
 // const carro = {
 //   rodas: 4,
@@ -11,14 +27,18 @@
 //     return this;
 //   },
 //   acelerar() {
-//     return this.marca + ' acelerou';
+//     return this.marca + ' acelerou'
 //   },
 //   buzinar() {
-//     return this.marca + ' buzinou';
+//     return this.marca + ' buzinou'
 //   }
 // }
 
 // const honda = Object.create(carro).init('Honda');
+
+// console.log(honda);
+// // honda.marca = 'Honda';
+// // honda.init('Honda');
 // console.log(honda.acelerar());
 
 // const ferrari = Object.create(carro).init('Ferrari');
@@ -30,7 +50,7 @@
 //     return 'acelerou';
 //   },
 //   buzinar() {
-//     return 'buzinou';
+//     return 'buzinou'
 //   },
 // }
 
@@ -39,73 +59,89 @@
 //   capacete: true,
 // }
 
-// Object.assign(moto, funcaoAutomovel);
+// Object.assign(moto, funcaoAutomovel, carro);
 
-// // console.log(moto)
+// console.log(moto)
+
 
 // const moto = {
-//   capacete: true,
+//   capacete: true
 // }
-
 
 // Object.defineProperties(moto, {
 //   rodas: {
+//     // value: 2,
+//     // configurable: false,
+//     // writable: true,
 //     enumerable: true,
 //     get() {
-//       return 2;
+//         return this._rodas;
 //     },
 //     set(valor) {
 //       this._rodas = valor * 4 + ' Total Rodas'
-//     }
+//     },
 //   }
-// })
+// });
 
 // console.log(moto)
 
 // const innerHeightConfig = Object.getOwnPropertyDescriptor(window, 'innerHeight');
 
-// Object.getOwnPropertyNames(Array.prototype)
+// console.log(Object.getOwnPropertyDescriptors(moto));
+
+// // Lista com métodos e propriedades e Array
+// console.log(Object.getOwnPropertyDescriptors(Array));
+
+// // Lista com métodos e propriedades do protótipo de Array
+// console.log(Object.getOwnPropertyDescriptors(Array.prototype));
+
+// // Puxa de uma única propriedade
+// console.log(Object.getOwnPropertyDescriptors(window));
+
+// console.log(Object.getOwnPropertyDescriptor(window, 'innerHeight'));
 
 
-
-// console.log(Object.getPrototypeOf(''));
+// console.log(Object.getPrototypeOf(frutas));
 // console.log(Array.prototype);
 
 // const frutas1 = ['Banana', 'Pêra'];
 // const frutas2 = ['Banana', 'Pêra'];
 
+// console.log(Object.is(frutas1, frutas2));
+
 // const novaFruta = frutas1;
 
 // novaFruta[0] = 'Uva';
 
-// console.log(frutas1)
-
-// Object.is(frutas1, novaFruta)
+// console.log(Object.is(frutas1, novaFruta));
 
 // const carro = {
 //   marca: 'Ford',
 //   ano: 2018,
 // }
 
-// Object.freeze(carro);
+// Object.seal(carro)
 // carro.portas = 4;
-// delete carro.marca;
+// delete carro.marca
 
 // carro.marca = 'Honda'
 
 // console.log(carro)
-// console.log(Object.isFrozen(carro))
+// console.log(Object.isFrozen(carro));
 
 const frutas = ['Banana', 'Uva'];
 const frase = 'Oi frase';
 const somar = function(a, b) {
-  return a + b;
+  return a + b
 };
-
 const carro = {
   marca: 'Ford'
 }
 
 console.log(frutas.toString());
+console.log(frase.toString());
+console.log(somar.toString());
+console.log(carro.toString());
 
-console.log(Object.prototype.toString.call(somar));
+// Verificar tipo de objeto
+console.log(Object.prototype.toString.call(frutas));
