284b18a755376d865a2ae5e7dbb20bfc6eedeb1c
TrevoYex <estevam.neto07@gmail.com>
2022-11-07 22:12:03 -0300

    Javascript - exercício function feito

--- JavaScript/04-objetos/04-objetos/0408-function/script.js
+++ JavaScript/04-objetos/04-objetos/0408-function/script.js
@@ -218,3 +218,21 @@ console.log(paragrafos);
 // Crie uma função que retorne novos elementos
 // html, com os seguintes parâmetros
 // tag, classe e conteudo.
+function criarElemento(tag, classe, conteudo) {
+  const elemento = document.createElement(tag);
+  classe ? elemento.classList.add(classe) : null;
+  conteudo ? elemento.innerHTML = conteudo : null;
+  return elemento; 
+}
+
+console.log(criarElemento('li', 'azul', 'Esse é o conteudo'))
+
+// Crie uma nova função utilizando a anterior como base
+// essa nova função deverá sempre criar h1 com a 
+// classe titulo. Porém o parâmetro conteudo continuará dinâmico
+const h1Titulo = criarElemento.bind(null, 'h1', 'titulo');
+
+const cursosJS = h1Titulo('Cursos de Javascript');
+const cursosHTML = h1Titulo('Cursos de HTML');
+
+console.log(cursosJS, cursosHTML);
