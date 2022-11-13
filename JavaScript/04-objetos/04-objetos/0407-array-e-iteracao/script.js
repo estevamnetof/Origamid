3d8bfd9330c41543fd58b05e1254629d2ebf7815
TrevoYex <estevam.neto07@gmail.com>
2022-11-13 00:15:50 -0300

    javascript - revisão ex mod 4

--- JavaScript/04-objetos/04-objetos/0407-array-e-iteracao/script.js
+++ JavaScript/04-objetos/04-objetos/0407-array-e-iteracao/script.js
@@ -1,13 +1,13 @@
 // Selecione cada curso e retorne uma array
-// com objetos contendo o título, descricao,
+// coms objetos contendo o título, decricao,
 // aulas e horas de cada curso
 const cursos = document.querySelectorAll('.curso');
 const arrayCursos = Array.from(cursos);
 
-const objetosCurso = arrayCursos.map((curso) => {
+const objetoCurso = arrayCursos.map((curso) => {
   const titulo = curso.querySelector('h1').innerText;
   const descricao = curso.querySelector('p').innerText;
-  const aulas = curso.querySelector('.aulas').innerText;
+  const aulas  = curso.querySelector('.aulas').innerText;
   const horas = curso.querySelector('.horas').innerText;
   return {
     titulo,
@@ -17,25 +17,25 @@ const objetosCurso = arrayCursos.map((curso) => {
   }
 })
 
-console.log(objetosCurso);
+console.log(objetoCurso);
 console.log(arrayCursos);
-
 
 // Retorne uma lista com os
 // números maiores que 100
 const numeros = [3, 44, 333, 23, 122, 322, 33];
-const maioresQue100 = numeros.filter(n => n > 100);
+const maiorQue100 = numeros.filter((numero) => {
+  return numero > 100;
+});
 
-console.log(maioresQue100)
+console.log(maiorQue100);
+
 
 // Verifique se Baixo faz parte
 // da lista de instrumentos e retorne true
-const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
-const possuiBaixo = instrumentos.some((item) => {
-  return item === 'Baixo';
-})
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado'];
+const temBaixo = instrumentos.some(x => x === 'Baixo');
 
-console.log(possuiBaixo);
+console.log(temBaixo);
 
 // Retorne o valor total das compras
 const compras = [
@@ -56,7 +56,7 @@ const compras = [
     preco: 'R$ 5,35'
   },
   {
-    item: 'Quejo',
+    item: 'Queijo',
     preco: 'R$ 10,60'
   },
   {
@@ -66,8 +66,8 @@ const compras = [
 ]
 
 const valorTotal = compras.reduce((acumulador, item) => {
-  const precoLimpo = +item.preco.replace('R$ ', '').replace(',', '.');
+  const precoLimpo = +item.preco.replace('R$', '').replace(',', '.');
   return acumulador + precoLimpo;
-}, 0)
+}, 0);
 
-console.log(valorTotal);
+console.log(valorTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'}));
