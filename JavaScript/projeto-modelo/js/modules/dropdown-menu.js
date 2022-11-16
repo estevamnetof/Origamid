5d79092c5a1cc37606e4ba7b9725dcac19bda3ec
TrevoYex <estevam.neto07@gmail.com>
2022-11-16 20:31:33 -0300

    Javascript - menu dropdown

---
+++ JavaScript/projeto-modelo/js/modules/dropdown-menu.js
@@ -0,0 +1,16 @@
+export default function initDropdownMenu() { 
+
+}
+
+const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+dropdownMenus.forEach(menu => {
+    menu.addEventListener('touchstart', handleClick);
+    menu.addEventListener('click', handleClick);
+});
+
+function handleClick(event) {
+    event.preventDefault();
+    // console.log(event);
+    this.classList.toggle('active');
+}
