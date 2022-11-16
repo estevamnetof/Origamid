ddfe717b033316efd8db2d1c0d1469a035f65924
TrevoYex <estevam.neto07@gmail.com>
2022-11-16 00:09:34 -0300

    Javascript - Modal

--- JavaScript/05-efeitos-no-dom/05-efeitos-no-dom/0503-modal/js/script.js
+++ JavaScript/05-efeitos-no-dom/05-efeitos-no-dom/0503-modal/js/script.js
@@ -2,9 +2,16 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
-import initModal from './modules/modal.js';
+import initModal from './modules/modal.js'
+// import * as teste from './modules/teste.js';
 
-initScrollSuave();
+// teste.teste1()
+// teste.teste2()
+// console.log(teste.senha);
+
+// console.log(this)
+
+initScrollSuave(); 
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
