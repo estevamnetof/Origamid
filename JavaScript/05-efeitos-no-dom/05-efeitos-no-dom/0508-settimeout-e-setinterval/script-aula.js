1ac5523a10ce167a9d742466348f7b0ac46d7934
TrevoYex <estevam.neto07@gmail.com>
2022-11-16 23:45:14 -0300

    Javascript - setTimeout e setInterval

--- JavaScript/05-efeitos-no-dom/05-efeitos-no-dom/0508-settimeout-e-setinterval/script-aula.js
+++ JavaScript/05-efeitos-no-dom/05-efeitos-no-dom/0508-settimeout-e-setinterval/script-aula.js
@@ -19,16 +19,52 @@
 // //   }, 300 * i)
 // // }
 
+// function loop(texto) {
+//   console.log(texto);
+// }
+// // setInterval(loop, 300, '300ms');
+
+// let i = 0;
+// const meuLoop = setInterval(() => {
+//   console.log(i++);
+//   if(i > 20) {
+//     clearInterval(meuLoop)
+//   }
+// }, 300);
+
+// ============================================
+
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', handleClick);
+
+// function handleClick() {
+//   setTimeout(() => {
+//     this.classList.add('active')
+//   }, 1000);
+// }
+
+// setTimeout(() => {
+//   console.log('Testando');
+// }, 0); 
+
+// for(let i = 0; i < 20; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000 * i)
+// }
+
 function loop(texto) {
   console.log(texto);
 }
-// setInterval(loop, 300, '300ms');
+// setInterval(loop, 1000, 'Passou 1s');
 
+// loop a cada segundo
 let i = 0;
 const meuLoop = setInterval(() => {
   console.log(i++);
   if(i > 20) {
-    clearInterval(meuLoop)
+    clearInterval(meuLoop);
   }
-}, 300);
+}, 1000);
 
