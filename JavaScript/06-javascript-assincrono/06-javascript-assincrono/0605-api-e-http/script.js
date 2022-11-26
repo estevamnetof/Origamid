b6ae95a8558966658a834d6ad9add61f991471dd
TrevoYex <estevam.neto07@gmail.com>
2022-11-26 19:49:49 -0300

    Javascript - API e HTTP

--- JavaScript/06-javascript-assincrono/06-javascript-assincrono/0605-api-e-http/script.js
+++ JavaScript/06-javascript-assincrono/06-javascript-assincrono/0605-api-e-http/script.js
@@ -4,7 +4,43 @@
 //   console.log(pokemon);
 // });
 
-const url = 'https://cors-anywhere.herokuapp.com/https://www.google.com';
+// const url = 'https://cors-anywhere.herokuapp.com/https://www.google.com';
+
+// fetch(url)
+// .then(response => response.text())
+// .then(text => console.log(text));
+
+// fetch('https://pokeapi.co/api/v2/pokemon/1/')
+// .then(r => r.json())
+// .then(pokemon => {
+//     console.log(pokemon);
+// });
+
+// const url = 'https://jsonplaceholder.typicode.com/posts';
+// const options = {
+//     method: 'POST',
+//     body: '{"title": "Javascript"}',
+//     headers: {
+//         "Content-Type": "application/json; charset=utf-8"
+//     }
+// }
+
+// fetch(url, options)
+// .then(response => response.json())
+// .then(json => console.log(json))
+
+// const url = 'https://jsonplaceholder.typicode.com/posts/2';
+// const options = {
+//     method: 'HEAD',
+// }
+
+// fetch(url, options)
+// .then(response => {
+//     console.log(response.headers.forEach(console.log))
+// })
+// .then(json => console.log(json));
+
+const url = 'https://www.google.com';
 
 fetch(url)
 .then(response => response.text())
