1b192a48f86a68f54a0c7c944f2093010b144f4b
TrevoYex <estevam.neto07@gmail.com>
2022-11-28 23:04:36 -0300

    Javascript - Async await

--- JavaScript/06-javascript-assincrono/06-javascript-assincrono/0606-async-await/script.js
+++ JavaScript/06-javascript-assincrono/06-javascript-assincrono/0606-async-await/script.js
@@ -1,3 +1,54 @@
+// async function puxarDados() {
+//   const responseDados = fetch('./dados.json');
+//   const responseClientes = fetch('./clientes.json');
+
+//   const jsonDados = await (await responseDados).json();
+//   const jsonClientes = await (await responseClientes).json();
+
+//   console.log(jsonDados)
+//   console.log(jsonClientes)
+// }
+
+// puxarDados();
+
+// async function asyncSemPromise() {
+//   // Console não irá esperar.
+//   await new Promise(resolve => {
+//     setTimeout(() => {
+//       console.log('Depois de 1s');
+//       resolve();
+//     }, 1000);
+//   })
+//   console.log('acabou');
+// }
+// asyncSemPromise();
+
+// async function puxarDados() {
+//   const responseDados = await fetch('./dados.json');
+//   const jsonDados = await responseDados.json()
+
+//   document.body.innerText = jsonDados.aula;
+//   console.log(jsonDados)
+// }
+
+// puxarDados()
+
+
+// async function puxarDados() {
+//   try {
+//     const responseDados = await fetch('./dados.json');
+//     const jsonDados = await responseDados.json();
+
+//     document.body.innerText = jsonDados.aula
+//   } catch(erro) {
+//     console.log(erro);
+//   }
+// }
+
+// puxarDados();
+
+
+
 async function puxarDados() {
   const responseDados = fetch('./dados.json');
   const responseClientes = fetch('./clientes.json');
@@ -11,14 +62,16 @@ async function puxarDados() {
 
 puxarDados();
 
+
 async function asyncSemPromise() {
   // Console não irá esperar.
   await new Promise(resolve => {
     setTimeout(() => {
-      console.log('Depois de 1s');
-      resolve();
+      console.log('Depois de 1s')
+      resolve()
     }, 1000);
   })
-  console.log('acabou');
+  console.log('acabou')
 }
+
 asyncSemPromise();
