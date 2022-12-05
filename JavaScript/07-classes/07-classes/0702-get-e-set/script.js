436d7468ee06dac21e73fc0a9f303d35591f89ec
TrevoYex <estevam.neto07@gmail.com>
2022-12-05 09:27:19 -0300

    Javascript - Get e Set

--- JavaScript/07-classes/07-classes/0702-get-e-set/script.js
+++ JavaScript/07-classes/07-classes/0702-get-e-set/script.js
@@ -1,11 +1,52 @@
+// const button = {
+//   get tamanho() {
+//     return this._numero || 100;
+//   },
+//   set tamanho(numero) {
+//     this._numero = numero * 200;
+//   }
+// }
+
+// const frutas = {
+//   lista: [],
+//   set nova(fruta) {
+//     this.lista.push(fruta);
+//   }
+// }
+
+// frutas.nova = 'Banana';
+// frutas.nova = 'Morango';
+
+// class Button {
+//   constructor(text, color) {
+//     this.text = text;
+//     this.color = color;
+//   }
+//   get element() {
+//     const type = this._elementType || 'button';
+//     const buttonElement = document.createElement(type);
+//     buttonElement.innerText = this.text;
+//     buttonElement.style.color = this.color;
+//     return buttonElement;
+//   }
+//   set element(type) {
+//     this._elementType = type;
+//   }
+// }
+
+// const blueButton = new Button('Comprar', 'blue');
+
+// =========================================================
+
 const button = {
   get tamanho() {
-    return this._numero || 100;
+    return this._numero || 100
   },
   set tamanho(numero) {
     this._numero = numero * 200;
   }
 }
+
 
 const frutas = {
   lista: [],
@@ -23,15 +64,15 @@ class Button {
     this.color = color;
   }
   get element() {
-    const type = this._elementType || 'button';
+    const type = this._elementType || 'Button';
     const buttonElement = document.createElement(type);
     buttonElement.innerText = this.text;
     buttonElement.style.color = this.color;
-    return buttonElement;
+    return buttonElement
   }
   set element(type) {
     this._elementType = type;
   }
 }
 
-const blueButton = new Button('Comprar', 'blue');
+const blueButton = new Button('Comprar', 'Blue');
