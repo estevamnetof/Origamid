327dba15591fac0036dd2a2454fd38c49d11ef1d
TrevoYex <estevam.neto07@gmail.com>
2022-12-14 10:38:10 -0300

    JavaScript - revisão de destructuring e rest e spread

--- JavaScript/08-mais-javascript/0804-destructuring/script-aula.js
+++ JavaScript/08-mais-javascript/0804-destructuring/script-aula.js
@@ -17,8 +17,92 @@
 
 // console.log(segundo);
 
-function handleKeyboard({key, keyCode}) {
-  console.log(key, keyCode);
+// function handleKeyboard({key, keyCode}) {
+//   console.log(key, keyCode);
+// }
+
+// document.addEventListener('keyup', handleKeyboard);
+
+// ==============================
+
+// Parâmetro Padrão
+function perimetroForma(lado, totalLados) {
+  totalLados = totalLados || 4;
+  return lado * totalLados;
 }
 
-document.addEventListener('keyup', handleKeyboard);
+// console.log(perimetroForma(10, 4));
+// console.log(perimetroForma(10));
+
+
+// Parâmetro Padrão ES6+ 
+function perimetroForma(lado, totalLados = 4) {
+  return lado * totalLados;
+}
+
+// console.log(perimetroForma(10, 5));
+// console.log(perimetroForma(10));
+
+
+// Arguments
+function perimetroForma(lado, totalLados = 4) {
+  console.log(arguments);
+  return lado * totalLados;
+}
+
+// console.log(perimetroForma(10, 5));
+// console.log(perimetroForma(10, 4, 20));
+
+
+// Parâmetro Rest
+function anunciarGanhadores(...ganhadores) {
+  ganhadores.forEach(ganhador => console.log(ganhador + ' ganhou'));
+}
+
+// anunciarGanhadores('Pedro', 'Marta', 'Maria');
+
+
+// Único Rest
+function anunciarGanhadores(premio, ...ganhadores) {
+  ganhadores.forEach(ganhador => console.log(ganhador + ' ganhou um ' + premio));
+}
+
+// anunciarGanhadores('Carro', 'Pedro', 'Marta', 'Maria');
+
+
+// Rest vs Arguments
+function anunciarGanhadores(premio, ...ganhadores) {
+  console.log(ganhadores);
+  console.log(arguments);
+}
+
+// anunciarGanhadores('Carro', 'Pedro', 'Marta', 'Maria');
+
+
+// Operador Spread
+const frutas = [' Banana', 'Uva', 'Morango'];
+const legumes = ['Cenoura', 'Batata'];
+
+const comidas = [...frutas, 'Pizza', ...legumes];
+
+// console.log(comidas);
+
+
+// Spread Argument
+const numeroMaximo = Math.max(4, 54, 4, 23, 356, 35, 25, 245);
+
+const listaNumeros = [2, 4, 56, 7, 24, 563, 6, 54];
+
+const numeroMaximoSpread = Math.max(...listaNumeros);
+console.log(numeroMaximoSpread);
+
+
+// Transformar em Array
+const btns = document.querySelectorAll('button');
+const btnsArray = [...btns];
+
+const frase = 'Isso é JavaScript';
+const fraseArray = [...frase];
+
+console.log(btnsArray);
+console.log(fraseArray);
