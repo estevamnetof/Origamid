52b79dcab08295f4187a10bdc5fe107bfccba7c4
TrevoYex <estevam.neto07@gmail.com>
2022-12-14 16:05:31 -0300

    Javascript - revisão loops e iterable

--- JavaScript/08-mais-javascript/0806-loops-e-iterable/script-aula.js
+++ JavaScript/08-mais-javascript/0806-loops-e-iterable/script-aula.js
@@ -51,3 +51,84 @@ let i = 0;
 do {
   console.log(i++);
 } while(i <= 20);
+
+// ==========================================
+
+// const frutas = ['Banana', 'Morango', 'Uva'];
+// const frase = 'Isso é JavaScript';
+// const fraseArray = [...frase];
+
+
+// fetch('https://pokeapi.co/api/v2/pokemon/')
+// .then(({headers}) => console.log(headers));
+
+// for(const fruta of frutas) {
+//   console.log(fruta);
+// }
+
+// OU (mesma coisa que usar forEach)
+
+// frutas.forEach(fruta => {
+//   console.log(fruta)
+// });
+
+// for(const char of frase) {
+//   console.log(char);
+// }
+
+// Spread e for...of 
+// const buttons = document.querySelectorAll('button');
+
+// for(const btn of buttons) {
+//   btn.style.background = 'blue';
+// }
+
+// console.log(...buttons);
+
+
+// Apenas Iteráveis
+// const carro = {
+//   marca: 'Honda',
+//   ano: 2018,
+// }
+
+// Erro, não é iterável
+// for(const propriedade of carro) {
+//   consolel.log(propriedade);
+// }
+
+
+// for...in
+// const carro = {
+//   marca: 'Honda',
+//   ano: 2018,
+// }
+
+// for(const propriedade in carro) {
+//   console.log(propriedade);
+// }
+
+
+
+// // Array e for...in
+// const frutas = ['Banana', 'Morango', 'Uva'];
+
+// for(const index in frutas) {
+//   console.log(frutas[index]);
+// }
+
+
+// // Chave e valor
+// // const btn = document.querySelector('button');
+// // const btnStyles = getComputedStyle(btn);
+
+// // for(const style in btnStyles) {
+// //   console.log(`${style}: ${btnStyles[style]}`)
+// // }
+
+
+// // Do / While
+// let i = 0;
+// do {
+//   console.log(i++);
+// } while (i <= 5);
--- JavaScript/08-mais-javascript/0806-loops-e-iterable/script.js
+++ JavaScript/08-mais-javascript/0806-loops-e-iterable/script.js
@@ -1,14 +1,14 @@
-// Crie 4 li's na página
-// Utilizando o for...of
-// adicione uma classe a cada li
-const itens = document.querySelectorAll('li');
-for(const i of itens) {
-  i.classList.add('active');
-}
+// // Crie 4 li's na página
+// // Utilizando o for...of
+// // adicione uma classe a cada li
+// const itens = document.querySelectorAll('li');
+// for(const i of itens) {
+//   i.classList.add('active');
+// }
 
-// Utilize o for...in para listar
-// todos as propriedades e valores
-// do objeto window
-for(const w in window) {
-  console.log(w + ': ' + window[w]);
-}
+// // Utilize o for...in para listar
+// // todos as propriedades e valores
+// // do objeto window
+// for(const w in window) {
+//   console.log(w + ': ' + window[w]);
+// }
