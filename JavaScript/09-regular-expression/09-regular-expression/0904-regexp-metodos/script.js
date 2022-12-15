9236165bcdb9bc9eb1f6fc72e2dc0d956fa85321
TrevoYex <estevam.neto07@gmail.com>
2022-12-15 00:00:15 -0300

    JavaScript - regexp e metodos

--- JavaScript/09-regular-expression/09-regular-expression/0904-regexp-metodos/script.js
+++ JavaScript/09-regular-expression/09-regular-expression/0904-regexp-metodos/script.js
@@ -1,53 +1,133 @@
-// const regexp = /\w+[-.]/gi;
+// // const regexp = /\w+[-.]/gi;
 
-// const regexp1 = new RegExp('\\w', 'gi');
-// console.log(frase.replace(regexp1, 'X'))
+// // const regexp1 = new RegExp('\\w', 'gi');
+// // console.log(frase.replace(regexp1, 'X'))
+
+// // const regexp = /Java/g;
+// // const frase = 'JavaScript e Java Linguagem 101, Java';
+
+// // let i = 1;
+// // while(regexp.test(frase)) {
+// //   console.log(i++, regexp.lastIndex)
+// // }
+
+// // const frase = 'JavaScript, TypeScript, CoffeeScript, Java, 200';
+
+// // const regexp = /[A-Za-z]+/g;
+
+// // const resultados = frase.split(regexp);
+
+// // console.log(resultados);
+
+// // const tags = `
+// // <ul>
+// //   <li>Item 1</li>
+// //   <li>Item 2</li>
+// // </ul>
+// // `;
+
+// // const regexp = /<li/g;
+
+// // const resultado = tags.replace(regexp, '$& class="ativa"');
+// // console.log(resultado);
+
+// const emails = `joao@homail.com.br
+// marta@ggmail.com.br
+// bruna@oulook.com.br
+// andre@origamid.com`
+
+// const regexp = /(\w+@)([\w.]+)/g
+
+// const resultado = emails.replace(regexp, function(...args) {
+//   if(args[2] === 'homail.com.br') {
+//     return args[1] + 'hotmail.com.br';
+//   } else if(args[2] === 'ggmail.com.br') {
+//     return args[1] + 'gmail.com.br';
+//   } else if(args[2] === 'oulook.com.br') {
+//     return args[1] + 'outlook.com.br';
+//   } else {
+//     return args[0]
+//   }
+// });
+
+// console.log(resultado)
+
+
+// ==================================================
+
+
+//  const regexp = /\w+/gi;
+
+//  const regexp1 = new RegExp('\\w+', 'gi');
+//  console.log(frase.replace(regexp, 'X'));
+//  console.log(frase.replace(regexp1, 'X'));
 
 // const regexp = /Java/g;
-// const frase = 'JavaScript e Java Linguagem 101, Java';
+
+// const frase = 'JavaScript e Java linguagem 101, Java';
 
 // let i = 1;
 // while(regexp.test(frase)) {
-//   console.log(i++, regexp.lastIndex)
+//   console.log(i++, regexp.lastIndex);
 // }
+
 
 // const frase = 'JavaScript, TypeScript, CoffeeScript, Java, 200';
 
-// const regexp = /[A-Za-z]+/g;
+// const regexp = /\w+/g;
 
+// let regexpResult;
+// while((regexpResult = regexp.exec(frase)) !== null) {
+//   console.log(regexpResult[0]);
+// }
+
+
+// const regexp = /[A-Za-z]/g;
 // const resultados = frase.split(regexp);
 
 // console.log(resultados);
+
 
 // const tags = `
 // <ul>
 //   <li>Item 1</li>
 //   <li>Item 2</li>
+//   <li>Item 3</li>
+// </ul>
+// `;
+
+// const regexp = /(?<=<\/?)\w+/g;
+
+// const resultado = tags.replace(regexp, '$& ---')
+// console.log(resultado);
+
+
+// const tags = `
+// <ul>
+//   <li>Item 1</li>
+//   <li>Item 2</li>
+//   <li>Item 3</li>
 // </ul>
 // `;
 
 // const regexp = /<li/g;
 
-// const resultado = tags.replace(regexp, '$& class="ativa"');
+// const resultado = tags.replace(regexp, '$& class="ativa"')
 // console.log(resultado);
 
-const emails = `joao@homail.com.br
-marta@ggmail.com.br
-bruna@oulook.com.br
-andre@origamid.com`
 
-const regexp = /(\w+@)([\w.]+)/g
+
+const emails = `
+empresa@email.com
+contato@email.com
+suporte@email.com
+`;
+
+const regexp = /(\w+@)[\w.]+/g;
 
 const resultado = emails.replace(regexp, function(...args) {
-  if(args[2] === 'homail.com.br') {
-    return args[1] + 'hotmail.com.br';
-  } else if(args[2] === 'ggmail.com.br') {
-    return args[1] + 'gmail.com.br';
-  } else if(args[2] === 'oulook.com.br') {
-    return args[1] + 'outlook.com.br';
-  } else {
-    return args[0]
-  }
+  console.log(args);
+  return '$1X'
 });
 
-console.log(resultado)
+console.log(resultado);
