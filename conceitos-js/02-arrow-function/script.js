f9113cda67244625f35078163721464a3afd6cf6
Estevam Neto <estevam.neto07@gmail.com>
2023-02-09 11:46:16 -0300

    conceitos js

---
+++ conceitos-js/02-arrow-function/script.js
@@ -0,0 +1,21 @@
+const upperName = name => {
+    return name.toUpperCase();
+}
+
+const countLetters = word => word.length;
+
+class Menu {
+    constructor(menu) {
+        this.menuElement = document.querySelector(menu);
+    }
+    addActiveEvent() {
+        this.menuElement.addEventListener('click', (event) => {
+            event.target.classList.add('active');
+        });
+    }
+}
+
+const menu = new Menu('.principal');
+menu.addActiveEvent();
+
+console.log(menu);
