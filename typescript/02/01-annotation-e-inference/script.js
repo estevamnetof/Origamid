bdb2c91edac60fe538c5a0b378a030bb22339626
estevamnetof <estevam.neto07@gmail.com>
2023-08-12 21:11:40 -0300

    TypeScript - String, Number e Boolean

--- typescript/02/01-annotation-e-inference/script.js
+++ typescript/02/01-annotation-e-inference/script.js
@@ -1,22 +1,45 @@
 "use strict";
-const produto = "Livro";
-const preco = 200;
-const carro = {
-    marca: 'Audi',
-    portas: 5,
-};
-const barato = 200 < 400 ? true : "produto caro";
-function somar(a, b) {
-    return a + b;
+// const produto = "Livro";
+// const preco = 200;
+// const carro = {
+//     marca: 'Audi',
+//     portas: 5,
+// }
+// const barato = 200 < 400 ? true : "produto caro";
+// function somar(a: number, b: number) {
+//     return a + b;
+// }
+// somar(4, 10);
+// const nintendo = {
+//     nome: 'Nintendo',
+//     preco: '2000',
+// }
+// function transformarPreco(produto: { nome: string; preco: string }) {
+//     produto.preco = 'R$ ' + produto.preco;
+//     return produto;
+// }   
+// const produtoNovo = transformarPreco(nintendo);
+// console.log(produtoNovo)
+// function normalizarTexto(texto: string) {
+//     return texto.trim().toLowerCase();
+// }
+// console.log(normalizarTexto(' DESigN    '))
+const input = document.querySelector('input');
+const total = localStorage.getItem('total');
+if (input && total) {
+    input.value = total;
+    calcularGanho(+input.value);
 }
-somar(4, 10);
-const nintendo = {
-    nome: 'Nintendo',
-    preco: '2000',
-};
-function transformarPreco(produto) {
-    produto.preco = 'R$ ' + produto.preco;
-    return produto;
+function calcularGanho(value) {
+    const p = document.querySelector('p');
+    if (p)
+        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
 }
-const produtoNovo = transformarPreco(nintendo);
-console.log(produtoNovo);
+function totalMudou() {
+    if (input) {
+        localStorage.setItem('total', input.value);
+        calcularGanho(+input.value);
+    }
+}
+if (input)
+    input.addEventListener('keyup', totalMudou);
---
+++ typescript/02/02-string-number-boolean/script.js
@@ -0,0 +1,18 @@
+"use strict";
+// const frase = 'Front End'
+// const preco = 500;
+// const condi = preco > 100;
+// console.log(typeof frase);
+// console.log(typeof preco);
+// console.log(typeof {});
+// if (typeof frase === 'string') {
+//     console.log('Frase é uma string');
+// } else {
+//     console.log('Frase não é uma string');
+// }
+const frase1 = new String('Front End');
+const frase2 = String('Front End');
+const frase3 = 'Front End';
+console.log(typeof frase1);
+console.log(typeof frase2);
+console.log(typeof frase3);
