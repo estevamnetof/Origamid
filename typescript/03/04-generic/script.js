a551b6c6ca3c52762d22909a04ae27339e33fe7f
estevamnetof <estevam.neto07@gmail.com>
2023-08-19 21:16:08 -0300

    TypeScript - generic parte 1

---
+++ typescript/03/04-generic/script.js
@@ -0,0 +1,31 @@
+"use strict";
+// function retorno<variavel>(a: variavel): variavel {
+//     return a;
+// }
+// console.log(retorno('A Game'));
+// console.log(retorno(200));
+// console.log(retorno(true));
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const frutas = ['Banana', 'Pêra', 'Uva', 'Laranja', 'Limão', 'Maçã'];
+// function firstFive<T>(lista: T[]): T[] {
+//     return lista.slice(0, 5);
+// }
+// console.log(firstFive(numeros));
+// console.log(firstFive(frutas));
+// function notNull<T>(arg: T) {
+//     if (arg !== null) return arg;
+//     else return null
+// }
+// console.log(notNull('André')?.toLowerCase());
+// console.log(notNull(200)?.toFixed());
+function tipoDado(a) {
+    const resultado = {
+        dado: a,
+        tipo: typeof a,
+    };
+    console.log(resultado);
+    return resultado;
+}
+tipoDado('teste');
+tipoDado(true);
+tipoDado(200);
