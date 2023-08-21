5feb14a402f18cc66f2a30b32d7afab46a44c943
estevamnetof <estevam.neto07@gmail.com>
2023-08-21 17:38:53 -0300

    TypeScript - revisões

--- typescript/testes/script.js
+++ typescript/testes/script.js
@@ -1,10 +1,21 @@
 "use strict";
-function ativarMenu(event) {
-    const elemento = event.currentTarget;
-    if (elemento instanceof HTMLElement) {
-        elemento.style.background = 'red';
-    }
+async function fetchProduct() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const data = await response.json();
+    console.log(data);
+    showProduct(data);
 }
-const button = document.querySelector('button');
-button?.addEventListener('click', ativarMenu);
-window.addEventListener('keydown', ativarMenu);
+fetchProduct();
+function showProduct(data) {
+    document.body.innerHTML = `
+        <div>
+        <h2>${data.nome}</h2>
+        <p>R$ ${data.preco}</p>
+        <p>${data.descricao}</p>
+        <p>Garantia: ${data.garantia}</p>
+        <p>Seguro de Acidentes: ${data.seguroAcidentes ? 'Sim' : 'Não'}</p>
+        <h3>Fabricante: ${data.empresaFabricante.nome}</h3>
+        <h3>Montadora: ${data.empresaMontadora.nome}</h3>
+        </div>
+    `;
+}
--- typescript/testes/script.ts
+++ typescript/testes/script.ts
@@ -1,13 +1,38 @@
+async function fetchProduct() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const data = await response.json();
+    console.log(data);
+    showProduct(data);
+}
+  
+fetchProduct();
 
-function ativarMenu(event: Event) {
-    const elemento = event.currentTarget;
-
-    if (elemento instanceof HTMLElement) {
-        elemento.style.background = 'red';
-    }
+interface Empresa {
+    nome: string;
+    fundacao: number;
+    paise: string;
 }
 
-const button = document.querySelector('button');
-button?.addEventListener('click', ativarMenu);
+interface Produtos {
+    nome: string;
+    preco: number;
+    descricao: string;
+    garantia: number;
+    seguroAcidentes: boolean;
+    empresaFabricante: Empresa;
+    empresaMontadora: Empresa;
+}
 
-window.addEventListener('keydown', ativarMenu);
+function showProduct(data: Produtos) {
+    document.body.innerHTML = `
+        <div>
+        <h2>${data.nome}</h2>
+        <p>R$ ${data.preco}</p>
+        <p>${data.descricao}</p>
+        <p>Garantia: ${data.garantia}</p>
+        <p>Seguro de Acidentes: ${data.seguroAcidentes ? 'Sim': 'Não'}</p>
+        <h3>Fabricante: ${data.empresaFabricante.nome}</h3>
+        <h3>Montadora: ${data.empresaMontadora.nome}</h3>
+        </div>
+    `;
+}
