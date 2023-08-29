a64124fe54fe8e501c723908b283adcf7612cc3c
estevamnetof <estevam.neto07@gmail.com>
2023-08-29 18:36:10 -0300

    TypeScript - unknown

--- typescript/testes/script.js
+++ typescript/testes/script.js
@@ -13,25 +13,3 @@ function typeGuard(value) {
 typeGuard('Origamid');
 typeGuard(200);
 typeGuard(document.body);
-// in
-const obj = {
-    nome: 'Origamid',
-};
-if ('nome' in obj) {
-    console.log('Sim');
-}
-async function fetchProduto() {
-    const response = await fetch('https://api.origamid.dev/json/notebook.json');
-    const json = await response.json();
-    handleProduto(json);
-}
-function handleProduto(data) {
-    if ('preco' in data) {
-        document.body.innerHTML += `
-            <p>Nome: ${data.nome}</p>
-            <p>Preço: R$${data.preco}</p>
-            <p>Descrição: ${data.descricao}</p>
-        `;
-    }
-}
-fetchProduto();
--- typescript/testes/script.ts
+++ typescript/testes/script.ts
@@ -1,4 +1,4 @@
-function typeGuard(value: any) {
+function typeGuard(value: unknown) {
     if (typeof value === 'string') {
         return value.toLocaleLowerCase();
     }
@@ -13,37 +13,3 @@ function typeGuard(value: any) {
 typeGuard('Origamid');
 typeGuard(200);
 typeGuard(document.body);
-
-// in
-const obj = {
-    nome: 'Origamid',
-}
-
-if ('nome' in obj) {
-    console.log('Sim');
-}
-
-// exemplo 2
-interface Produto {
-    nome: string;
-    preco: number;
-    descricao: string;
-}
-
-async function fetchProduto() {
-    const response = await fetch('https://api.origamid.dev/json/notebook.json');
-    const json = await response.json();
-    handleProduto(json);
-}
-
-function handleProduto(data: Produto) {
-    if ('preco' in data) {
-        document.body.innerHTML += `
-            <p>Nome: ${data.nome}</p>
-            <p>Preço: R$${data.preco}</p>
-            <p>Descrição: ${data.descricao}</p>
-        `
-    }
-}
-
-fetchProduto();
