a47fbc259e7c85a1c9a3116cda79637187315c23
estevamnetof <estevam.neto07@gmail.com>
2023-08-29 17:36:54 -0300

    TypeScript - revisão de generics

--- typescript/testes/script.js
+++ typescript/testes/script.js
@@ -1,7 +1,52 @@
 "use strict";
-function $(seletor) {
-    return document.querySelector(seletor);
+// exemplo 1
+function retorno(a) {
+    return a;
 }
-$('a')?.href;
-$('video')?.volume;
-$('.teste')?.innerHTML;
+console.log(retorno('A Game').charAt(0));
+console.log(retorno(200).toFixed());
+// exemplo 2
+function firstFive(lista) {
+    return lista.slice(0, 5);
+}
+const numeros = [1, 3, 4, 5, 3, 20, 3, 4, 5];
+const frutas = ['Banana', 'Pêra', 'Uva', 'Laranja', 'Limão'];
+const five1 = firstFive(numeros);
+const five2 = firstFive(frutas);
+console.log(five1);
+console.log(five2);
+// exemplo 3
+function notNull(arg) {
+    if (arg !== null)
+        return arg;
+    else
+        return null;
+}
+console.log(notNull(200)?.toFixed());
+// Extends
+// exemplo 1   
+function extractText(el) {
+    return el.innerText;
+}
+// const link = document.querySelector('a');
+// if (link) {
+//     console.log(extractText(link));
+// }
+// exemplo 2
+function $(selector) {
+    return document.querySelector(selector);
+}
+// const link = $<HTMLAnchorElement>('a')?.href;
+// console.log(link);
+// Métodos
+const link = document.querySelector('.link');
+link?.href;
+async function getData(url) {
+    const response = await fetch(url);
+    return await response.json();
+}
+async function handleData() {
+    const notebook = await getData('https://api.origamid.dev/json/notebook.json');
+    console.log(notebook.nome);
+}
+handleData();
--- typescript/testes/script.ts
+++ typescript/testes/script.ts
@@ -1,11 +1,70 @@
-function $(seletor: 'video'): HTMLVideoElement | null;
-function $(seletor: 'div'): HTMLDivElement | null;
-function $(seletor: 'a'): HTMLAnchorElement | null;
-function $(seletor: string): Element | null;
-function $(seletor: string): Element | null {
-    return document.querySelector(seletor);
+// exemplo 1
+function retorno<Tipo>(a: Tipo): Tipo {
+    return a;
 }
 
-$('a')?.href;
-$('video')?.volume;
-$('.teste')?.innerHTML;
+console.log(retorno('A Game').charAt(0));
+console.log(retorno(200).toFixed());
+
+// exemplo 2
+function firstFive<Tipo>(lista: Tipo[]): Tipo[] {
+    return lista.slice(0, 5);
+}
+
+const numeros = [1, 3, 4, 5, 3, 20, 3, 4, 5];
+const frutas = ['Banana', 'Pêra', 'Uva', 'Laranja', 'Limão'];
+
+const five1 = firstFive(numeros);
+const five2 = firstFive(frutas);
+
+console.log(five1);
+console.log(five2);
+
+// exemplo 3
+function notNull<T>(arg: T) {
+    if (arg !== null) return arg;
+    else return null;
+}
+
+console.log(notNull(200)?.toFixed());
+
+// Extends
+// exemplo 1   
+function extractText<T extends HTMLElement>(el: T): string {
+    return el.innerText;
+}
+
+// const link = document.querySelector('a');
+
+// if (link) {
+//     console.log(extractText(link));
+// }
+
+// exemplo 2
+function $<T extends Element>(selector: string): T | null {
+    return document.querySelector(selector);
+}
+
+// const link = $<HTMLAnchorElement>('a')?.href;
+// console.log(link);
+
+// Métodos
+const link = document.querySelector<HTMLAnchorElement>('.link');
+link?.href;
+
+async function getData<T>(url: string): Promise<T> {
+    const response = await fetch(url);
+    return await response.json();
+}
+
+interface Notebook {
+    nome: string;
+    preco: number;
+}
+
+async function handleData() {
+    const notebook = await getData<Notebook>('https://api.origamid.dev/json/notebook.json');
+    console.log(notebook.nome);
+}
+
+handleData();
