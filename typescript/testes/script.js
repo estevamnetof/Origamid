32c0aee7f577e38b35ebef3097cc64e5aeae4ea3
estevamnetof <estevam.neto07@gmail.com>
2023-09-05 19:25:55 -0300

    TypeScript - revisão Generics

--- typescript/testes/script.js
+++ typescript/testes/script.js
@@ -1,84 +1,12 @@
 "use strict";
-// Array
-// async function fetchCursos() {
-//     const response = await fetch('https://api.origamid.dev/json/cursos.json');
-//     const json = await response.json();
-//     handleCursos(json);
-// }
-// fetchCursos();
-// function handleCursos(data: unknown) {
-//     if (data instanceof Array) {
-//         console.log('É instância de array');
-//     }
-//     if (Array.isArray(data)) {
-//         console.log('É array');
-//     }
-// }
-// Type Predicate
-function isString(value) {
-    return typeof value === 'string';
+function retorno(a) {
+    return a;
 }
-function handleData(data) {
-    if (isString(data)) {
-        data.toUpperCase();
-    }
+retorno('A Game').charAt(0);
+function extractText(el) {
+    return el.innerText;
 }
-// console.log(handleData('Origamid'));
-// Objetos
-// async function fetchProduto() {
-//     const response = await fetch('https://api.origamid.dev/json/notebook.json');
-//     const json = await response.json();
-//     handleProduto(json);
-// }
-// fetchProduto();
-// interface Produto {
-//     nome: string;
-//     preco: number;
-//     descricao: string;
-// }
-// function isProduto(value: unknown): value is Produto {
-//     if (value && typeof value === 'object' && 'nome' in value && 'preco' in value) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-// function handleProduto(data: unknown) {
-//     if (isProduto(data)) {
-//         document.body.innerHTML += `
-//             <h2>${data.nome}</h2>
-//             <p>R$ ${data.preco}</p>
-//             <p>${data.descricao}</p>
-//         `
-//     };
-// }
-// Exercício
-async function fetchCursos() {
-    const response = await fetch('https://api.origamid.dev/json/cursos.json');
-    const json = await response.json();
-    console.log(json);
-    handleCursos(json);
-}
-fetchCursos();
-function isCurso(value) {
-    if (value && typeof value === 'object' && 'nome' in value && 'aulas' in value && 'gratuito' in value && 'tags' in value) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-function handleCursos(data) {
-    if (data instanceof Array) {
-        data.filter(isCurso).forEach(item => {
-            document.body.innerHTML += `
-                <div>
-                    <h2>${item.nome}</h2>
-                    <p>Aulas: ${item.aulas}</p>
-                    <p>Gratuito: ${item.gratuito ? 'Sim' : 'Não'}</p>
-                    <p>${item.tags.join(', ')}</p>
-                </div>
-            `;
-        });
-    }
+const link = document.querySelector('a');
+if (link) {
+    console.log(extractText(link));
 }
--- typescript/testes/script.ts
+++ typescript/testes/script.ts
@@ -1,103 +1,15 @@
-// Array
-// async function fetchCursos() {
-//     const response = await fetch('https://api.origamid.dev/json/cursos.json');
-//     const json = await response.json();
-//     handleCursos(json);
-// }
-
-// fetchCursos();
-
-// function handleCursos(data: unknown) {
-//     if (data instanceof Array) {
-//         console.log('É instância de array');
-//     }
-//     if (Array.isArray(data)) {
-//         console.log('É array');
-//     }
-// }
-
-// Type Predicate
-function isString(value: unknown): value is string {
-    return typeof value === 'string';
+function retorno<Tipo>(a: Tipo): Tipo {
+    return a;
 }
 
-function handleData(data: unknown) {
-    if (isString(data)) {
-        data.toUpperCase();
-    }
+retorno('A Game').charAt(0);
+
+function extractText<T extends HTMLElement>(el: T): string  {
+    return el.innerText;
 }
 
-// console.log(handleData('Origamid'));
+const link = document.querySelector('a');
 
-// Objetos
-// async function fetchProduto() {
-//     const response = await fetch('https://api.origamid.dev/json/notebook.json');
-//     const json = await response.json();
-//     handleProduto(json);
-// }
-
-// fetchProduto();
-
-// interface Produto {
-//     nome: string;
-//     preco: number;
-//     descricao: string;
-// }
-
-// function isProduto(value: unknown): value is Produto {
-//     if (value && typeof value === 'object' && 'nome' in value && 'preco' in value) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-
-// function handleProduto(data: unknown) {
-//     if (isProduto(data)) {
-//         document.body.innerHTML += `
-//             <h2>${data.nome}</h2>
-//             <p>R$ ${data.preco}</p>
-//             <p>${data.descricao}</p>
-//         `
-//     };
-// }
-
-// Exercício
-async function fetchCursos() {
-    const response = await fetch('https://api.origamid.dev/json/cursos.json');
-    const json = await response.json();
-    console.log(json);
-    handleCursos(json);
-}
-
-fetchCursos();
-
-interface Curso {
-    nome: string;
-    aulas: number;
-    gratuito: boolean;
-    tags: string[];
-}
-
-function isCurso(value: unknown): value is Curso {
-    if (value && typeof value === 'object' && 'nome' in value && 'aulas' in value && 'gratuito' in value && 'tags' in value) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function handleCursos(data: unknown) {
-    if (data instanceof Array) {
-        data.filter(isCurso).forEach(item => {
-            document.body.innerHTML += `
-                <div>
-                    <h2>${item.nome}</h2>
-                    <p>Aulas: ${item.aulas}</p>
-                    <p>Gratuito: ${item.gratuito ? 'Sim' : 'Não'}</p>
-                    <p>${item.tags.join(', ')}</p>
-                </div>
-            `
-        });
-    }
+if (link) {
+    console.log(extractText(link))
 }
