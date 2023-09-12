4dc0b82988bd14cbe0555ecf26ef329693d9a856
estevamnetof <estevam.neto07@gmail.com>
2023-09-12 18:42:22 -0300

    TypeScript - revisão User Type Guard

--- typescript/testes/script.js
+++ typescript/testes/script.js
@@ -1,12 +1,46 @@
 "use strict";
-function retorno(a) {
-    return a;
+async function fetchCursos() {
+    const response = await fetch('https://api.origamid.dev/json/cursos.json');
+    const json = await response.json();
+    handleCursos(json);
 }
-retorno('A Game').charAt(0);
-function extractText(el) {
-    return el.innerText;
+fetchCursos();
+function handleCursos(data) {
+    if (data instanceof Array) {
+        console.log('É instância de Array');
+    }
+    if (Array.isArray(data)) {
+        console.log('É Array');
+    }
 }
-const link = document.querySelector('a');
-if (link) {
-    console.log(extractText(link));
+function isString(value) {
+    return typeof value === 'string';
+}
+function handleData(data) {
+    if (isString(data)) {
+        data.toUpperCase();
+    }
+}
+handleData('Origamid');
+async function fetchProduto() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const json = await response.json();
+    handleProduto(json);
+}
+fetchProduto();
+function isProduto(value) {
+    if (value && typeof value === 'object' && 'nome' in value && 'preco' in value) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function handleProduto(data) {
+    if (isProduto(data)) {
+        document.body.innerHTML += `
+            <h2>${data.nome}</h2>
+            <p>R$ ${data.preco}</p>
+        `;
+    }
 }
--- typescript/testes/script.ts
+++ typescript/testes/script.ts
@@ -1,15 +1,60 @@
-function retorno<Tipo>(a: Tipo): Tipo {
-    return a;
+async function fetchCursos() {
+    const response = await fetch('https://api.origamid.dev/json/cursos.json');
+    const json = await response.json();
+    handleCursos(json);
 }
 
-retorno('A Game').charAt(0);
+fetchCursos();
 
-function extractText<T extends HTMLElement>(el: T): string  {
-    return el.innerText;
+function handleCursos(data: unknown) {
+    if (data instanceof Array) {
+        console.log('É instância de Array');
+    }
+    if (Array.isArray(data)) {
+        console.log('É Array');
+    }
 }
 
-const link = document.querySelector('a');
 
-if (link) {
-    console.log(extractText(link))
+function isString(value: unknown): value is string {
+    return typeof value === 'string';
+}
+
+function handleData(data: unknown) {
+    if (isString(data)) {
+        data.toUpperCase();
+    }
+}
+
+handleData('Origamid');
+
+
+async function fetchProduto() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const json = await response.json();
+    handleProduto(json);
+}
+
+fetchProduto();
+
+interface Produto {
+    nome: string;
+    preco: number;
+}
+
+function isProduto(value: unknown): value is Produto {
+    if (value && typeof value === 'object' && 'nome' in value && 'preco' in value) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function handleProduto(data: unknown) {
+    if (isProduto(data)) {
+        document.body.innerHTML += `
+            <h2>${data.nome}</h2>
+            <p>R$ ${data.preco}</p>
+        `
+    }
 }
