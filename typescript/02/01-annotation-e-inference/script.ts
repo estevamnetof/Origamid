ac82e79f900c5416834458f1c8e268044eb50f42
estevamnetof <estevam.neto07@gmail.com>
2023-11-02 19:14:47 -0300

    revisando

--- typescript/02/01-annotation-e-inference/script.ts
+++ typescript/02/01-annotation-e-inference/script.ts
@@ -1,59 +1,32 @@
-// const produto = "Livro";
-// const preco = 200;
+let produto: string = 'Livro';
+let preco: number = 200;
 
-// const carro = {
-//     marca: 'Audi',
-//     portas: 5,
-// }
-
-// const barato = 200 < 400 ? true : "produto caro";
-
-// function somar(a: number, b: number) {
-//     return a + b;
-// }
-
-// somar(4, 10);
-
-// const nintendo = {
-//     nome: 'Nintendo',
-//     preco: '2000',
-// }
-
-// function transformarPreco(produto: { nome: string; preco: string }) {
-//     produto.preco = 'R$ ' + produto.preco;
-//     return produto;
-// }   
-
-// const produtoNovo = transformarPreco(nintendo);
-
-// console.log(produtoNovo)
-
-
-// function normalizarTexto(texto: string) {
-//     return texto.trim().toLowerCase();
-// }
-
-// console.log(normalizarTexto(' DESigN    '))
-  
-
-const input = document.querySelector('input');
-
-const total = localStorage.getItem('total');
-if (input&& total) {
-    input.value = total;
-    calcularGanho(+input.value);
+const carro: {
+    marca: string;
+    portas: number;
+} = {
+    marca: 'Audi',
+    portas: 5,
 }
 
-function calcularGanho(value: number) {
-    const p = document.querySelector('p');
-    if (p) p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+const barato = 200 < 400 ? true : 'produto caro';
+
+function somar(a: number, b: number) {
+    return a + b;
 }
 
-function totalMudou() {
-    if (input) {
-        localStorage.setItem('total', input.value);
-        calcularGanho(+input.value);
-    }
+somar(4, 10);
+
+const nintendo = {
+    nome: 'Nintendo',
+    preco: '2000',
 }
 
-if (input) input.addEventListener('keyup', totalMudou);
+function transformarPreco(produto: { nome: string; preco: string }) {
+    produto.preco = "R$ " + produto.preco;
+    return produto;
+}
+
+const produtoNovo = transformarPreco(nintendo);
+
+console.log(produtoNovo);
