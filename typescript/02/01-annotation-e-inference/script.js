b0b73647db6c9508602f03b02faf61260a3ff458
estevamnetof <estevam.neto07@gmail.com>
2023-11-02 19:39:59 -0300

    revisão

--- typescript/02/01-annotation-e-inference/script.js
+++ typescript/02/01-annotation-e-inference/script.js
@@ -20,3 +20,30 @@ function transformarPreco(produto) {
 }
 const produtoNovo = transformarPreco(nintendo);
 console.log(produtoNovo);
+// Exercicio 1
+function normalizarTexto(texto) {
+    return texto.trim().toLowerCase();
+}
+console.log(normalizarTexto('  oasdnOJDOAJDO '));
+// Exercicio 2
+const input = document.querySelector('input');
+const total = localStorage.getItem('total');
+if (input && total) {
+    input.value = total;
+    calcularGanho(Number(input.value));
+}
+function calcularGanho(value) {
+    const p = document.querySelector('p');
+    if (p) {
+        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+    }
+}
+function totalMudou() {
+    if (input) {
+        localStorage.setItem('total', input.value);
+        calcularGanho(Number(input.value));
+    }
+}
+if (input) {
+    input.addEventListener('keyup', totalMudou);
+}
--- typescript/02/01-annotation-e-inference/script.ts
+++ typescript/02/01-annotation-e-inference/script.ts
@@ -30,3 +30,38 @@ function transformarPreco(produto: { nome: string; preco: string }) {
 const produtoNovo = transformarPreco(nintendo);
 
 console.log(produtoNovo);
+
+// Exercicio 1
+function normalizarTexto(texto: string) {
+    return texto.trim().toLowerCase();
+}
+
+console.log(normalizarTexto('  oasdnOJDOAJDO '));
+
+// Exercicio 2
+
+const input = document.querySelector('input');
+
+const total = localStorage.getItem('total');
+if (input && total) {
+    input.value = total;
+    calcularGanho(Number(input.value));
+}
+
+function calcularGanho(value: number) {
+  const p = document.querySelector('p');
+  if (p) {
+     p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+    }
+}
+
+function totalMudou() {
+    if (input) {
+        localStorage.setItem('total', input.value);
+        calcularGanho(Number(input.value));
+    }
+}
+
+if (input) {
+    input.addEventListener('keyup', totalMudou);
+}
