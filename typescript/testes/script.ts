c8230d87b2fbc0b26a63625ee2d9e32ff368f8cb
estevamnetof <estevam.neto07@gmail.com>
2023-11-06 22:16:24 -0300

    revisão

--- typescript/testes/script.ts
+++ typescript/testes/script.ts
@@ -1,9 +1,63 @@
-function toNumber(value: number | string) {
-    if (typeof value === 'number') {
-        return value;
-    } else if (typeof value === 'string') {
-        return Number(value);
-    } else {
-        throw 'Value deve ser um número ou uma string';
-    }
-}
+// // function toNumber(value: number | string) {
+// //     if (typeof value === 'number') {
+// //         return value;
+// //     } else if (typeof value === 'string') {
+// //         return Number(value);
+// //     } else {
+// //         throw 'Value deve ser um número ou uma string';
+// //     }
+// // }
+
+// interface Produto {
+//     nome: string;
+//     preco: number;
+//     teclado: boolean;
+// }
+
+// function preencherDados(dados: Produto) {
+//     document.body.innerHTML += `
+//         <div>
+//             <h2>${dados.nome}</h2>
+//             <p>${dados.preco}</p>
+//             <p>Inclui teclado: ${dados.teclado ? 'Sim' : 'Não'}</p>
+//         </div>
+//     `;
+// }
+
+// preencherDados({
+//     nome: 'Computador',
+//     preco: 2000,
+//     teclado: false
+// });
+
+// preencherDados({
+//     nome: 'Notebook',
+//     preco: 1500,
+//     teclado: true
+// });
+
+async function fetchProduct() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const data = await response.json();
+    showProduct(data);
+    console.log(data);
+  }
+  
+  fetchProduct();
+
+  interface Produto {
+    nome: string;
+    preco: number;
+    descricao: string;
+  }
+  
+  function showProduct(data: Produto) {
+    document.body.innerHTML = `
+      <div>
+        <h2>${data.nome}</h2>
+        <p>R$ ${data.preco}</p>
+        <p>${data.descricao}</p>
+      </div>
+    `;
+  }
+  
