737557319ee25c863005b345c82c2532dff21cc2
estevamnetof <estevam.neto07@gmail.com>
2023-11-06 22:00:01 -0300

    revisão

--- typescript/testes/script.js
+++ typescript/testes/script.js
@@ -1,46 +1,12 @@
 "use strict";
-async function fetchCursos() {
-    const response = await fetch('https://api.origamid.dev/json/cursos.json');
-    const json = await response.json();
-    handleCursos(json);
-}
-fetchCursos();
-function handleCursos(data) {
-    if (data instanceof Array) {
-        console.log('É instância de Array');
+function toNumber(value) {
+    if (typeof value === 'number') {
+        return value;
     }
-    if (Array.isArray(data)) {
-        console.log('É Array');
-    }
-}
-function isString(value) {
-    return typeof value === 'string';
-}
-function handleData(data) {
-    if (isString(data)) {
-        data.toUpperCase();
-    }
-}
-handleData('Origamid');
-async function fetchProduto() {
-    const response = await fetch('https://api.origamid.dev/json/notebook.json');
-    const json = await response.json();
-    handleProduto(json);
-}
-fetchProduto();
-function isProduto(value) {
-    if (value && typeof value === 'object' && 'nome' in value && 'preco' in value) {
-        return true;
+    else if (typeof value === 'string') {
+        return Number(value);
     }
     else {
-        return false;
-    }
-}
-function handleProduto(data) {
-    if (isProduto(data)) {
-        document.body.innerHTML += `
-            <h2>${data.nome}</h2>
-            <p>R$ ${data.preco}</p>
-        `;
+        throw 'Value deve ser um número ou uma string';
     }
 }
--- typescript/testes/script.ts
+++ typescript/testes/script.ts
@@ -1,60 +1,9 @@
-async function fetchCursos() {
-    const response = await fetch('https://api.origamid.dev/json/cursos.json');
-    const json = await response.json();
-    handleCursos(json);
-}
-
-fetchCursos();
-
-function handleCursos(data: unknown) {
-    if (data instanceof Array) {
-        console.log('É instância de Array');
-    }
-    if (Array.isArray(data)) {
-        console.log('É Array');
-    }
-}
-
-
-function isString(value: unknown): value is string {
-    return typeof value === 'string';
-}
-
-function handleData(data: unknown) {
-    if (isString(data)) {
-        data.toUpperCase();
-    }
-}
-
-handleData('Origamid');
-
-
-async function fetchProduto() {
-    const response = await fetch('https://api.origamid.dev/json/notebook.json');
-    const json = await response.json();
-    handleProduto(json);
-}
-
-fetchProduto();
-
-interface Produto {
-    nome: string;
-    preco: number;
-}
-
-function isProduto(value: unknown): value is Produto {
-    if (value && typeof value === 'object' && 'nome' in value && 'preco' in value) {
-        return true;
+function toNumber(value: number | string) {
+    if (typeof value === 'number') {
+        return value;
+    } else if (typeof value === 'string') {
+        return Number(value);
     } else {
-        return false;
-    }
-}
-
-function handleProduto(data: unknown) {
-    if (isProduto(data)) {
-        document.body.innerHTML += `
-            <h2>${data.nome}</h2>
-            <p>R$ ${data.preco}</p>
-        `
+        throw 'Value deve ser um número ou uma string';
     }
 }
