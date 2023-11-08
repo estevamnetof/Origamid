cb0388441715d4db02dbf664808de1146417200a
estevamnetof <estevam.neto07@gmail.com>
2023-11-07 22:31:25 -0300

    revisão

--- typescript/testes/script.js
+++ typescript/testes/script.js
@@ -1,32 +1,13 @@
 "use strict";
-const numeros = [10, 30, 40, 5, 3, 30];
-const valores = [10, 'Taxas', 40, 'Produto', 3, 30];
-function maiorQue10(data) {
-    return data.filter((n) => n > 10);
+class Produto {
+    nome;
+    preco;
+    constructor(nome, preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+    precoReal() {
+        return `R$ ${this.preco}`;
+    }
 }
-console.log(maiorQue10(numeros));
-async function fetchCursos() {
-    const response = await fetch('https://api.origamid.dev/json/cursos.json');
-    const data = await response.json();
-    mostrarCursos(data);
-    console.log(data);
-}
-fetchCursos();
-function mostrarCursos(cursos) {
-    cursos.forEach((curso) => {
-        let color;
-        if (curso.nivel === 'iniciante') {
-            color = 'blue';
-        }
-        else if (curso.nivel === 'avancado') {
-            color = 'red';
-        }
-        document.body.innerHTML += `
-            <div>
-                <h2 style="color: ${color};">${curso.nome}</h2>
-                <p>${curso.horas} Horas</p>
-                <p>${curso.nivel}</p>
-            </div>
-        `;
-    });
-}
+const livro = new Produto('A Guerra dos Tronos', 200);
--- typescript/testes/script.ts
+++ typescript/testes/script.ts
@@ -1,44 +1,13 @@
-const numeros = [10, 30, 40, 5, 3, 30];
-const valores = [10, 'Taxas', 40, 'Produto', 3, 30];
-
-function maiorQue10(data: number[]) {
-    return data.filter((n) => n > 10);
-}
-
-console.log(maiorQue10(numeros));
-
-
-async function fetchCursos() {
-    const response = await fetch('https://api.origamid.dev/json/cursos.json');
-    const data = await response.json();
-    mostrarCursos(data);
-    console.log(data);
-}
-  
-fetchCursos();
-
-interface Produto {
+class Produto {
     nome: string;
-    horas: number;
-    nivel: 'iniciante' | 'avancado';
+    preco: number;
+    constructor(nome: string, preco: number) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+    precoReal() {
+        return `R$ ${this.preco}`;
+    }
 }
 
-function mostrarCursos(cursos: Produto[]) {
-    cursos.forEach((curso) => {
-        let color;
-        if (curso.nivel === 'iniciante') {
-            color = 'blue';
-        } else if (curso.nivel === 'avancado') {
-            color = 'red';
-        }
-
-        document.body.innerHTML += `
-            <div>
-                <h2 style="color: ${color};">${curso.nome}</h2>
-                <p>${curso.horas} Horas</p>
-                <p>${curso.nivel}</p>
-            </div>
-        `
-    })
-}
-  
+const livro = new Produto('A Guerra dos Tronos', 200);
