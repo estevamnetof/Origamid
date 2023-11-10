be3002b813b943407f25203ecb8ae14c38224fe5
estevamnetof <estevam.neto07@gmail.com>
2023-11-10 20:08:12 -0300

    revisão

--- typescript/testes/script.ts
+++ typescript/testes/script.ts
@@ -1,13 +1,2 @@
 class Produto {
-    nome: string;
-    preco: number;
-    constructor(nome: string, preco: number) {
-        this.nome = nome;
-        this.preco = preco;
-    }
-    precoReal() {
-        return `R$ ${this.preco}`;
-    }
 }
-
-const livro = new Produto('A Guerra dos Tronos', 200);
